511263110027af17fdeee823b38afa2ccc24bf5e
Andre Capelache <andre@webfatorial.com>
2017-05-17 00:46:56 +0200

    Add vhost on nginx-fpm

--- nginx-fpm/7.1-fpm/scripts/start.sh
+++ nginx-fpm/7.1-fpm/scripts/start.sh
@@ -74,9 +74,7 @@ if [ -f /var/www/html/conf/nginx/nginx-site-ssl.conf ]; then
   cp /var/www/html/conf/nginx/nginx-site-ssl.conf /etc/nginx/sites-available/default-ssl.conf
 fi
 
-if [ "$NGINX_VHOST" = "1" ] ; then
-  cp /var/www/html/conf/*.conf /etc/nginx/sites-enabled/
-fi
+cp /var/www/html/conf/*.conf /etc/nginx/sites-enabled/
 
 # Display PHP error's or not
 if [[ "$ERRORS" != "1" ]] ; then
